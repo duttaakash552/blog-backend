818dc4ab338230629fbb9556cdf625200c38a061
duttaakash552 <duttaakash552@gmail.com>
2026-07-06 17:39:16 +0530

    Change in index

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const URL = process.env.URL;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+mongoose.connect(URL).then(() => {
+    console.log('Database connected successfully');
+}).catch((err) => {
+    console.log('Database connection failed', err);
 });
