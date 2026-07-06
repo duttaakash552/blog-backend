b20ec53e076a11ab014273f799ff60221b681179
duttaakash552 <duttaakash552@gmail.com>
2026-07-06 19:13:54 +0530

    Changes in index file.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 const URL = process.env.URL;
+
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
